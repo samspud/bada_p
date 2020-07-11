c87f742b24a53f78fb392c8ad7919d064114c3ba
Suheon Lee <45480288+samspud@users.noreply.github.com>
2020-07-12 07:37:21 +0900

    Updates

---
+++ precache-manifest.f31b12903fdb0e0c4dbe38446ae9322c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4856de39636a328b85b03307d66b247d",
+    "url": "/bada-project/index.html"
+  },
+  {
+    "revision": "df9f3b24687d1276d7c9",
+    "url": "/bada-project/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "b8f73b53cf3cb3e4e2e3",
+    "url": "/bada-project/static/js/2.c033013e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bada-project/static/js/2.c033013e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9f3b24687d1276d7c9",
+    "url": "/bada-project/static/js/main.3789ca37.chunk.js"
+  },
+  {
+    "revision": "329a42754aacc849b9b7",
+    "url": "/bada-project/static/js/runtime-main.c8faae93.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/bada-project/static/media/logo.ee7cd8ed.svg"
+  }
+]);
