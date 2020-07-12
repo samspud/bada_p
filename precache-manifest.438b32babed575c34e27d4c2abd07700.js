3b180110cdd51924282ef6aeca1f59044ac39f4d
Suheon Lee <45480288+samspud@users.noreply.github.com>
2020-07-12 10:51:54 +0900

    Updates

---
+++ precache-manifest.438b32babed575c34e27d4c2abd07700.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3322e8b337da7d2c6d7b9954a2b11875",
+    "url": "/bada_p/index.html"
+  },
+  {
+    "revision": "84f5efba0aef53d70201",
+    "url": "/bada_p/static/css/main.2da64ebc.chunk.css"
+  },
+  {
+    "revision": "55dc45561f0f1ce71db1",
+    "url": "/bada_p/static/js/2.ca7bbc8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bada_p/static/js/2.ca7bbc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f5efba0aef53d70201",
+    "url": "/bada_p/static/js/main.d3a81b95.chunk.js"
+  },
+  {
+    "revision": "ebadc255a2237d8511e5",
+    "url": "/bada_p/static/js/runtime-main.3d911b1c.js"
+  },
+  {
+    "revision": "90ee5688f5abd6f6bb354d7ce9165f5a",
+    "url": "/bada_p/static/media/C_12.90ee5688.png"
+  },
+  {
+    "revision": "bcc3fb4d56f15e738899ee801bd94ed3",
+    "url": "/bada_p/static/media/Main.bcc3fb4d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bada_p/precache-manifest.3d0747052a350205098b4eb71619e9da.js"
+  "/bada_p/precache-manifest.438b32babed575c34e27d4c2abd07700.js"
 );
 
 self.addEventListener('message', (event) => {
